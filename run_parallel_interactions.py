bbc2cccdf45f768bf833008fdde6c912bc6881a3
badinkajink <williamxie00@gmail.com>
2025-03-02 13:50:09 -0700

    6x40 calls is the right balance ith ink

--- run_parallel_interactions.py
+++ run_parallel_interactions.py
@@ -4,9 +4,8 @@ import subprocess
 # Define ranges for chunks
 # chunks = [(0, 60), (60, 120), (120, 180), (180, 240)]
 # generate 24 chunks of 10
-chunks = [(i*20, (i+1)*20) for i in range(12)]
+chunks = [(i*40, (i+1)*40) for i in range(6)]
 fp = 'conversations/gpt-4o-mini/run_2'
-
 def run_chunk(start, end):
     subprocess.run(["python", "run_structured_interaction.py", str(start), str(end), fp])
 
